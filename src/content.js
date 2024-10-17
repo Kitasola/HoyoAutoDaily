2c405108483b4edaf0003dad9445128420dd9446
Kitasola <sorakita0221@gmail.com>
2024-10-17 05:17:00 +0000

    auto close web bonus page

--- src/content.js
+++ src/content.js
@@ -2,7 +2,14 @@ import { WebBonusDOM } from "./web_bonus_dom"
 
 const start = async () => {
     const helper = new WebBonusDOM()
-    helper.dailyCheckIn()
+    await helper.dailyCheckIn()
+
+    // 完了通知
+    await chrome.tabs.query({ url: 'https://act.hoyolab.com/ys/event/signin-sea-v3/index.html?act_id=e202102251931481' }, (tabs) => {
+        tabs.forEach(tab => {
+            chrome.tabs.remove(tab.id)
+        });
+    });
 }
 
 start()
