a5563a796ef484377cf13841dcd06ba687edb954
Kitasola <sorakita0221@gmail.com>
2024-10-24 16:14:10 +0000

    add api url

--- src/config.js
+++ src/config.js
@@ -5,23 +5,35 @@ const config = {
         'HG0': {
             'id': 'HG0',
             'name': 'Genshin Impact',
+            'act_id': 'e202102251931481',
             'enable': false,
-            'url': null,
+            'page_url': null,
+            'info_url': 'https://sg-hk4e-api.hoyolab.com/event/sol/info',
+            'sign_url': 'https://sg-hk4e-api.hoyolab.com/event/sol/sign',
             'checking': false,
+            'signname': '',
         },
         'HG1': {
             'id': 'HG1',
             'name': 'Hokai Star Rail',
+            'act_id': 'e202303301540311',
             'enable': false,
-            'url': null,
+            'page_url': null,
+            'info_url': 'https://sg-public-api.hoyolab.com/event/luna/os/info',
+            'sign_url': 'https://sg-public-api.hoyolab.com/event/luna/os/sign',
             'checking': false,
+            'signname': '',
         },
         'HG2': {
             'id': 'HG2',
             'name': 'Zenless Zone Zero',
+            'act_id': 'e202406031448091',
             'enable': false,
-            'url': null,
+            'page_url': null,
+            'info_url': 'https://sg-public-api.hoyolab.com/event/luna/zzz/os/info',
+            'sign_url': 'https://sg-public-api.hoyolab.com/event/luna/zzz/os/sign',
             'checking': false,
+            'signname': 'zzz',
         },
     },
     'lastdate': new Date(0).toDateString(),
@@ -49,21 +61,21 @@ export const changeEnableGame = async (id, enable) => {
     const games_info = await getGamesInfo()
     games_info[id].enable = (enable === true)
     await chrome.storage.sync.set({ 'games_info': games_info, })
-    logger.debug(`${games_info[id].name}'s info change ${games_info[id].enable}`)
+    logger.debug(`${id}'s info change ${games_info[id].enable}`)
 };
 
 export const onCheckingGame = async (id) => {
     const games_info = await getGamesInfo()
     games_info[id].checking = true
     await chrome.storage.sync.set({ 'games_info': games_info, })
-    logger.debug(`${games_info[id].name}'s checking on`)
+    logger.debug(`${id}'s checking on`)
 };
 
 export const offCheckingGame = async (id) => {
     const games_info = await getGamesInfo()
     games_info[id].checking = false
     await chrome.storage.sync.set({ 'games_info': games_info, })
-    logger.debug(`${games_info[id].name}'s checking off`)
+    logger.debug(`${id}'s checking off`)
 };
 
 export const getLastdate = async () => {
@@ -107,23 +119,21 @@ export const changeCheckInTime = async (h, m) => {
     logger.debug(`check in time change "${h}:${m}"`)
 }
 
-export const updateGameURL = async () => {
+export const updateGamePageURL = async () => {
     const manifest = chrome.runtime.getManifest()
     const games_info = await getGamesInfo()
 
     for (const game of Object.values(games_info)) {
-        const target = manifest.content_scripts.find(
-            (script) => script.js.some(
-                (file) => file.includes(game.id)
-            )
+        const target = manifest.content_scripts[0].matches.find(
+            (url) => url.includes(game.act_id)
         )
         if (target) {
-            games_info[game.id].url = target.matches[0]
+            games_info[game.id].page_url = target
         }
     }
 
     await chrome.storage.sync.set({ 'games_info': games_info, })
-    logger.debug(`update games_info url`)
+    logger.debug(`update games_info page_url`)
 }
 
 export const getAlarmStat = async () => {
